40be7fb5ca3308524c50d548b09c9b54b6c0236a
Andreas Ritter <azuri.ritter@gmail.com>
2016-01-26 15:03:37 -0500

    MAPPER FOR BIAS AND DARK CHANGED TO BIAS-PF%(site)s%(category)s_%(ccd)02d.fits

--- config/pfs/flat.py
+++ config/pfs/flat.py
@@ -4,6 +4,8 @@ from lsst.utils import getPackageDir
 
 config.load(os.path.join(getPackageDir("obs_subaru"), "config", "pfs", "isr.py"))
 
-from pfs.drp.stella.detrends import PfsFlatCombineTask
-config.combination.retarget(PfsFlatCombineTask)
+#from pfs.drp.stella.detrends import FlatCombineTask
+#config.combination.retarget(FlatCombineTask)
+#from pfs.drp.stella.detrends import PfsFlatCombineTask
+#config.combination.retarget(PfsFlatCombineTask)
 #config.combination.load(os.path.join(os.environ['OBS_SUBARU_DIR'], 'config', 'pfs', 'vignette.py'))
